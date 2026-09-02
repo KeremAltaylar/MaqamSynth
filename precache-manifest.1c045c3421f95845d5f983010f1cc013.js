9c58adee3fff65bd5870de6465b766549f26ad0b
Kerem Altaylar <keremaltaylar@gmail.com>
2026-09-02 21:12:47 +0300

    Updates

--- precache-manifest.1c045c3421f95845d5f983010f1cc013.js
+++ precache-manifest.1c045c3421f95845d5f983010f1cc013.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9da5ffd084f006e2e57351e6a23215e4",
+    "revision": "4e6fdd5ecd447dd1e5db578abd642c42",
     "url": "/MaqamSynth/index.html"
   },
   {
-    "revision": "5d7848c1ca3899ffc0ba",
+    "revision": "6857013c30b07a6fd082",
     "url": "/MaqamSynth/static/css/main.2ad417d3.chunk.css"
   },
   {
-    "revision": "397f474aea11c50157dd",
-    "url": "/MaqamSynth/static/js/2.791d85f5.chunk.js"
+    "revision": "884f908e577496368f7b",
+    "url": "/MaqamSynth/static/js/2.b47b3bed.chunk.js"
   },
   {
-    "revision": "5d7848c1ca3899ffc0ba",
-    "url": "/MaqamSynth/static/js/main.bf737443.chunk.js"
+    "revision": "6857013c30b07a6fd082",
+    "url": "/MaqamSynth/static/js/main.9e2cbd8e.chunk.js"
   },
   {
     "revision": "3cee22269e5b47dbe0fd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MaqamSynth/precache-manifest.073e6777a3d2b81cec27b37f6ae989f2.js"
+  "/MaqamSynth/precache-manifest.1c045c3421f95845d5f983010f1cc013.js"
 );
 
 self.addEventListener('message', (event) => {
